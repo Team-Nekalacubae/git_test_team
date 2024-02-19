68457b57d7e6e9f486f5ce052ea54a4b6a700b8b
Java_Literal <zziny725@icloud.com>
2024-02-19 17:37:31 +0900

    bySu

---
+++ src/main/java/com/ohgiraffers/Application.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("안녕");
+
+
+
+    }
+
+}
